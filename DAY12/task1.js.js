c3c91826e870f85dde542cf5334ee35c443f728e
jeyasubha-26 <kit27.ad027@gmail.com>
2025-06-17 15:57:28 +0530

    Update task1.js

---
+++ DAY12/task1.js.js
@@ -0,0 +1,20 @@
+function swap(p1,p2)
+{
+    var t=p1;
+    p1=p2;
+    p2=t;
+    console.log(p1,p2);
+}
+let value=swap(10,20);
+console.log(value)
+function test()
+{
+    var a=10;
+    var b=20;
+    var c=a;
+    a=b;
+    b=c;
+    console.log("the value of a is "+a);
+    console.log("the value of b is"+b);
+}
+test()
