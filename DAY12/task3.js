c6ff72fe99085c06dfe72df051efba6c5ec72882
jeyasubha-26 <kit27.ad027@gmail.com>
2025-06-17 15:58:56 +0530

    Update task3.js

---
+++ DAY12/task3.js
@@ -0,0 +1,6 @@
+let name="subha"
+let age=19
+let isStudent=true
+console.log("my name is "+name+" , Iam "+age+" years old."+" Student: "+isStudent)
+const obj={name:"subha",age:19,isstudent:true}
+console.log(obj)
