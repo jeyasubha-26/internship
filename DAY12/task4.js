9d9f0031c4c2f9a832b37955279134432f001634
jeyasubha <jeyasubhaganesan2006@gmail.com>
2025-06-17 15:51:42 +0530

    firstcommit

---
+++ DAY12/task4.js
@@ -0,0 +1,3 @@
+let x=prompt("Enter a value:");
+let y=prompt("Enter a value:");
+alert("Add:"+ (x+y) +"\nsub:"+(x-y)+"\n multiplication:"+(x*y)+"\n division:"+(x/y));
---
+++ DAY12/task5.js
@@ -0,0 +1,7 @@
+function sum(x,y)
+{
+    return x+y;
+}
+let x=Number(prompt("Enter a value:"));
+let y=Number(prompt("Enter a value:"));
+alert(sum(x,y));
